dc5ff8757db1b76388df8654c483e3e9fbcbd8d7
Sheeraz Ali <sheerazali656@gmail.com>
2020-07-02 08:32:50 -0700

    Initial Commit

---
+++ ComboBox_To_ComboBox/ComboBox_To_ComboBox/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ComboBox_To_ComboBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxCountries = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBoxCities = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxCountries
+            // 
+            this.comboBoxCountries.FormattingEnabled = true;
+            this.comboBoxCountries.Location = new System.Drawing.Point(75, 77);
+            this.comboBoxCountries.Name = "comboBoxCountries";
+            this.comboBoxCountries.Size = new System.Drawing.Size(234, 21);
+            this.comboBoxCountries.TabIndex = 0;
+            this.comboBoxCountries.SelectedIndexChanged += new System.EventHandler(this.comboBoxCountries_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Countries";
+            // 
+            // comboBoxCities
+            // 
+            this.comboBoxCities.FormattingEnabled = true;
+            this.comboBoxCities.Location = new System.Drawing.Point(75, 157);
+            this.comboBoxCities.Name = "comboBoxCities";
+            this.comboBoxCities.Size = new System.Drawing.Size(234, 21);
+            this.comboBoxCities.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(72, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cities";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 312);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBoxCities);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxCountries);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxCountries;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBoxCities;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ComboBox_To_ComboBox/ComboBox_To_ComboBox/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComboBox_To_ComboBox
+{
+    public partial class Form1 : Form
+    {
+        string cs = ConfigurationManager.ConnectionStrings["csdb"].ConnectionString;
+        DataRow dr;
+        public Form1()
+        {
+            InitializeComponent();
+            BindWithComboBoxCountries();
+        }
+
+
+        public void BindWithComboBoxCountries() {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Countries";
+            SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+            DataTable data = new DataTable();
+            adapter.Fill(data);
+            dr = data.NewRow();
+            dr.ItemArray = new object[] {0, "-- Select Country --" };
+            data.Rows.InsertAt(dr, 0);
+            comboBoxCountries.DisplayMember = "Name";
+            comboBoxCountries.ValueMember = "Id";
+            comboBoxCountries.DataSource = data;
+
+
+        
+        }
+
+        private void comboBoxCountries_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBoxCountries.SelectedValue.ToString() != null) {
+
+                int id = Convert.ToInt32(comboBoxCountries.SelectedValue.ToString());
+                BindCitiesWithComboBox(id);
+            }
+        }
+
+        public void BindCitiesWithComboBox(int id) {
+
+            SqlConnection conn = new SqlConnection(cs);
+            string query = "select * from Cities where Id = @id";
+            SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+            sda.SelectCommand.Parameters.AddWithValue("@id", id);
+            DataTable data = new DataTable();
+            sda.Fill(data);
+            dr = data.NewRow();
+            dr.ItemArray = new object[] {0, "-- Select City --" };
+            data.Rows.InsertAt(dr, 0);
+            comboBoxCities.DisplayMember = "Name";
+            comboBoxCities.ValueMember = "C_Id";
+            comboBoxCities.DataSource = data;
+
+
+
+        }
+    }
+}
